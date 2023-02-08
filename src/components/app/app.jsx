7ebcae1698f15be47abce2a1d6463df0da0ee209
Sanat6301 <sanatabdunazarov@gmail.com>
2023-02-08 18:39:07 +0500

    fix: dynamic routing

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,17 @@
 import { Routes, Route} from "react-router-dom";
-import { Box } from "@mui/material"
-import { Main, Channel } from "../"
+import { Box } from "@mui/material";
+import { Main, Channel,VideoDetail,Search,Navbar} from "../";
 
 
 const App = () => {
   return(
     <Box>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Main/>} />
-        <Route path="/channel" element={<Channel/>} />
+        <Route path="/channel/:id" element={<Channel/>} />
+        <Route path="/video/:id" element={<VideoDetail/>} />
+        <Route path="/search/:id" element={<Search/>} />
       </Routes>
     </Box>
   )
