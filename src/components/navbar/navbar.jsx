8bd7886490767cddfc4fad534a71f7d291555091
Sanat6301 <sanatabdunazarov@gmail.com>
2023-02-09 18:54:22 +0500

    fix: navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Stack, Box} from "@mui/material";
+import { Link } from "react-router-dom";
+import { logo } from "../../constants";
+import { colors } from "../../constants/colors";
+import { SearchBar } from "../"; 
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack 
+      direction={'row'}
+      alignItems={'center'} 
+      justifyContent={'space-around'} 
+      p={2} 
+      sx={
+        {position: 'sticky', top: 0, zIndex: 999, background: colors.primary}}
+      >      
+      <Link to={"/"} >
+      <img src={logo} alt='' height={60}/>
+      </Link>
+      <SearchBar/>
+      <Box/>
+    </Stack>
   )
 }
 
 export default Navbar;
+
