2129563fd6f523c09ee900f8696dd0e673e0c6af
sanat630 <sanatabdunazarov@gmail.com>
2023-02-11 14:30:58 +0500

    fix: category

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,11 +1,21 @@
-import {Link} from 'react-router-dom';
-import {Button} from '@mui/material';
+import { useState } from 'react';
+import {Box, Container, Typography} from '@mui/material';
+import { Stack } from '@mui/system';
+import { colors } from '../../constants/colors';
 
 const Main = () => {
+  const [selectCategory, setselectCategory] = useState("New")
   return (
-    <Link to={"/channel"}>
-        <Button>Channel</Button>
-    </Link>
+    <Stack>
+      Category
+      <Box p={2} sx={{ height:'90vh'}}>
+        <Container maxWidth={'90%'}>
+          <Typography variant='h4' fontWeight={"bold"} mb={2}>
+            {selectCategory} <span style={{color: colors.secondary}}>videos</span>
+          </Typography>
+        </Container>
+      </Box>
+    </Stack>
   )
 }
 
